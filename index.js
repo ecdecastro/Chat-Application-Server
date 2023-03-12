d9befa5babe432431867767b89a1bf8ec68034aa
ecdecastro <eugenecdecastro@gmail.com>
2023-03-13 04:39:38 +0800

    chat-application-server cors origin to *

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ mongoose
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://sample-chat-application.netlify.app/",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
